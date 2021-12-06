e89699768d706019373b5754d1f7bb067bb38135
nidaa <nidaasallam@gmail.com>
2021-12-06 02:02:37 +0200

    7-15 finished

--- js/ratings.js
+++ js/ratings.js
@@ -9,6 +9,18 @@ const collect_ratings = () => {
  const elements = document.querySelectorAll(".rating");
 
  elements.forEach(element => {
-     
+    rating = parseInt(element.id.replace('star','')); 
+    ratings.count += parseInt(element.value);
+    ratings.sum += parseInt(element.value)*rating;
+    if(ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
  });
+ return ratings;
 }
+
+document.addEventListener('change', (e) => {
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+});
